d0491047284001ad58eaa1a525e8e82427342210
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-08-30 14:30:48 +1200

    fixed and added bolos

--- routes/createListing.js
+++ routes/createListing.js
@@ -8,11 +8,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-
-    console.log(req.body.buttonType);
-
+  var data = fs.readFile('../MOYB-challange-2018/public/Data/Listings.json', 'utf8',(err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      obj = JSON.parse(data);
+      obj.push(
+        {
+          "title": "Yeet Style Oven",
+          "description": "oven, Pretty shit, has had much use",
+          "price": 20000,
+          "category": "kitchen",
+          "featured": true,
+          "type": "listing"
+        }
+      );
+      fs.writeFile('../MOYB-challange-2018/public/Data/Listings.json', JSON.stringify(obj), 'utf8', (err, data) => {
+        res.render('createListing');
+      });
+    }
+  });
 });
-
-
 
 module.exports = router;
