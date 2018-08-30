5d0acbfade853a0a2886d2726b9b997785afaa12
tu dinh <dinhtu@ecs.vuw.ac.nz>
2018-08-30 14:30:34 +1200

    push

--- routes/createListing.js
+++ routes/createListing.js
@@ -2,15 +2,26 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+
+var data = fs.readFile('../MOYB-challange-2018/public/Data/Listings.json', 'utf8',(err, fileContent) => {
+    if( err ) {
+        console.log(err);
+    }
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('createListing');
 });
 
 router.post('/', function(req, res, next) {
+    var data = fs.readFile('../MOYB-challange-2018/public/Data/Listings.json', 'utf8',(err, fileContent) => {
+        if( err ) {
+            console.log(err);
+        }
 
-    console.log(req.body.buttonType);
+    });
 
+    console.log(data);
 });
 
 
