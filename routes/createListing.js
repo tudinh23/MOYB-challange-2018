d8eb5547a90517f251d34e8e1e87e6d8a179e3ac
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-08-30 14:34:10 +1200

    oh fixed the merge heh heh

--- routes/createListing.js
+++ routes/createListing.js
@@ -14,7 +14,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-<<<<<<< HEAD
   var data = fs.readFile('../MOYB-challange-2018/public/Data/Listings.json', 'utf8',(err, data) => {
     if(err) {
       console.log(err);
@@ -36,16 +35,6 @@ router.post('/', function(req, res, next) {
       });
     }
   });
-=======
-    var data = fs.readFile('../MOYB-challange-2018/public/Data/Listings.json', 'utf8',(err, fileContent) => {
-        if( err ) {
-            console.log(err);
-        }
-
-    });
-
-    console.log(data);
->>>>>>> 5d0acbfade853a0a2886d2726b9b997785afaa12
 });
 
 module.exports = router;
