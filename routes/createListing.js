542701d5a6e5d721c0064011857613cbdfb21ad6
tu dinh <dinhtu@ecs.vuw.ac.nz>
2018-09-25 11:03:11 +1200

    pihsing up changes

--- routes/createListing.js
+++ routes/createListing.js
@@ -14,27 +14,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  var data = fs.readFile('../MOYB-challange-2018/public/Data/Listings.json', 'utf8',(err, data) => {
-    if(err) {
-      console.log(err);
-    }
-    else {
-      obj = JSON.parse(data);
-      obj.push(
-        {
-          "title": "Yeet Style Oven",
-          "description": "oven, Pretty shit, has had much use",
-          "price": 20000,
-          "category": "kitchen",
-          "featured": true,
-          "type": "listing"
-        }
-      );
-      fs.writeFile('../MOYB-challange-2018/public/Data/Listings.json', JSON.stringify(obj), 'utf8', (err, data) => {
-        res.render('createListing');
-      });
-    }
-  });
+    res.redirect('/myAccount');
 });
 
 module.exports = router;
